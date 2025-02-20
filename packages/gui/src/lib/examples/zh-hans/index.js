e482137489830e34154da9d901e55128ddf34d9e
Yeqin Gong <gon@gyeq.in>
2025-02-20 15:45:49 +0800

    更新案例

--- packages/gui/src/lib/examples/zh-hans/index.js
+++ packages/gui/src/lib/examples/zh-hans/index.js
@@ -1,5 +1,6 @@
 import weather from './weather';
 import idioms from './idioms';
 import astronautClock from './astronaut-clock';
+import game2048 from './game2048';
 
-export default [astronautClock, weather, idioms];
+export default [astronautClock, weather, idioms, game2048];
