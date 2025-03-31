8d98d0bb4002208a1536469e2676a98484eb56b5
Yeqin Gong <gon@gyeq.in>
2025-03-31 22:01:20 +0800

    优化自制积木转换代码

--- packages/blocks/src/generators/emulator/procedures.js
+++ packages/blocks/src/generators/emulator/procedures.js
@@ -5,22 +5,23 @@ const proto = EmulatorGenerator.prototype;
 
 proto['procedures_definition'] = function (block) {
   const myBlock = block.childBlocks_[0];
-  const funcName = this.getVariableName(myBlock.getProcCode(), ScratchBlocks.Procedures.NAME_TYPE);
+  const funcName = this.getFunctionName(myBlock.getProcCode());
   const args = myBlock.childBlocks_.map((argBlock) => this.getVariableName(argBlock.getFieldValue('VALUE')));
-  args.push('done');
+  const argsCode = args.length > 0 ? `${args.join(', ')}, done` : 'done';
 
-  let funcCode = this.HAT_CALLBACK;
-  funcCode = funcCode.replace('(done) => {\n', `(${args.join(', ')}) => {\n`);
-  funcCode = funcCode.replace('= runtime.warpMode;\n', `= ${myBlock.warp_};\n`);
-  return `runtime.define('procedure:${funcName}', ${funcCode});\n`;
+  let branchCode = this.statementToCode(block);
+  branchCode = this.addEventTrap(branchCode, block.id)
+    .replace('(done) => {\n', `(${argsCode}) => {\n`)
+    .replace('= runtime.warpMode;\n', `= ${myBlock.warp_};\n`);
+
+  const code = `runtime.define('procedure:${funcName}', ${branchCode});\n`;
+  return code;
 };
 
 proto['procedures_call'] = function (block) {
-  const funcName = this.getVariableName(block.getProcCode(), ScratchBlocks.Procedures.NAME_TYPE);
+  const funcName = this.getFunctionName(block.getProcCode());
   const args = block.argumentIds_.map((arg) => this.valueToCode(block, arg, this.ORDER_NONE));
   const argsCode = args.length > 0 ? `, ${args.join(', ')}` : '';
-
-  let code = '';
-  code += `await runtime.call('procedure:${funcName}'${argsCode});\n`;
+  const code = `await runtime.call('procedure:${funcName}'${argsCode});\n`;
   return code;
 };
