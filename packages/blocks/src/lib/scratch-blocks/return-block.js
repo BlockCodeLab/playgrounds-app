df35ac532028400840ae9cad79e7ae23e86022da
Yeqin Gong <gon@gyeq.in>
2025-08-18 14:42:01 +0800

    修复自制积木返回积木的BUG

--- packages/blocks/src/lib/scratch-blocks/return-block.js
+++ packages/blocks/src/lib/scratch-blocks/return-block.js
@@ -432,10 +432,10 @@ function getAllProcedureReturnTypes(workspace) {
   const blocks = workspace.getTopBlocks(false);
   for (let i = 0; i < blocks.length; i++) {
     if (blocks[i].type == ScratchBlocks.PROCEDURES_DEFINITION_BLOCK_TYPE) {
-      const procCode = blocks[i].getInput('custom_block').connection.targetBlock().getProcCode();
+      const procCode = blocks[i].getInput('custom_block').connection.targetBlock()?.getProcCode?.();
       // To match behavior of getDefineBlock, if multiple instances of this procedure are
       // defined, only use the first one.
-      if (!Object.prototype.hasOwnProperty.call(result, procCode)) {
+      if (procCode && !Object.prototype.hasOwnProperty.call(result, procCode)) {
         result[procCode] = getBlockReturnType(blocks[i]);
       }
     }
