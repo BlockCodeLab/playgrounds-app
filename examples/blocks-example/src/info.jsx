4854f6bc6030d9455d03bd5390aeadf879b88bcd
Yeqin Gong <yeqin.gong@qq.com>
2026-02-13 15:02:18 +0800

    更新依赖版本

--- examples/blocks-example/src/info.jsx
+++ examples/blocks-example/src/info.jsx
@@ -2,25 +2,6 @@ import { addLocalesMessages, Text } from '@blockcode/core';
 import featureImage from './feature.png';
 import iconImage from './icon.png';
 
-// 多语言信息，只包含信息显示需要的多语言，积木的多语言另外添加
-addLocalesMessages({
-  en: {
-    'blocks.example.name': 'Example',
-    'blocks.example.description': 'Blocks extension example.',
-    'blocks.example.collaborator': 'Your Name',
-  },
-  'zh-Hans': {
-    'blocks.example.name': '案例',
-    'blocks.example.description': '图形积木扩展案例。',
-    'blocks.example.collaborator': '你的名字',
-  },
-  'zh-Hant': {
-    'blocks.example.name': '案例',
-    'blocks.example.description': '圖形積木擴展案例。',
-    'blocks.example.collaborator': '你的名字',
-  },
-});
-
 export default {
   image: featureImage,
   icon: iconImage,
@@ -45,3 +26,22 @@ export default {
   // 过滤条件设置
   tags: ['example'],
 };
+
+// 多语言信息，只包含信息显示需要的多语言，积木的多语言另外添加
+addLocalesMessages({
+  en: {
+    'blocks.example.name': 'Example',
+    'blocks.example.description': 'Blocks extension example.',
+    'blocks.example.collaborator': 'Your Name',
+  },
+  'zh-Hans': {
+    'blocks.example.name': '案例',
+    'blocks.example.description': '图形积木扩展案例。',
+    'blocks.example.collaborator': '你的名字',
+  },
+  'zh-Hant': {
+    'blocks.example.name': '案例',
+    'blocks.example.description': '圖形積木擴展案例。',
+    'blocks.example.collaborator': '你的名字',
+  },
+});
--- examples/editor-example/src/info.jsx
+++ examples/editor-example/src/info.jsx
@@ -2,28 +2,8 @@ import { addLocalesMessages, Text } from '@blockcode/core';
 import { version } from '../package.json';
 import featureImage from './feature.png';
 
-// 多语言信息，只包含信息显示需要的多语言，编辑器更多的多语言另外添加
-addLocalesMessages({
-  en: {
-    'example.name': 'Example',
-    'example.description': 'Editor example.',
-    'example.collaborator': 'Your Name',
-  },
-  'zh-Hans': {
-    'example.name': '案例',
-    'example.description': '编辑器案例。',
-    'example.collaborator': '你的名字',
-  },
-  'zh-Hant': {
-    'example.name': '案例',
-    'example.description': '編輯器案例。',
-    'example.collaborator': '你的名字',
-  },
-});
-
 export default {
   version,
-  // sortIndex: 1xxxx, // 社区组 10000～19999
   image: featureImage, // 主题图片
   name: (
     <Text
@@ -45,3 +25,22 @@ export default {
   ),
   blocksRequired: true, // 图形积木编程（仅供显示）
 };
+
+// 多语言信息，只包含信息显示需要的多语言，编辑器更多的多语言另外添加
+addLocalesMessages({
+  en: {
+    'example.name': 'Example',
+    'example.description': 'Editor example.',
+    'example.collaborator': 'Your Name',
+  },
+  'zh-Hans': {
+    'example.name': '案例',
+    'example.description': '编辑器案例。',
+    'example.collaborator': '你的名字',
+  },
+  'zh-Hant': {
+    'example.name': '案例',
+    'example.description': '編輯器案例。',
+    'example.collaborator': '你的名字',
+  },
+});
