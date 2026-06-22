3154cecc4b26a348f9e80e87e6741f9ecae4a0a3
Yeqin Gong <yeqin.gong@qq.com>
2026-06-22 17:07:50 +0800

    移除写作组件

--- packages/write/src/index.js
+++
@@ -1,9 +0,0 @@
-import './l10n';
-
-import 'vditor/dist/index.css';
-
-export { MarkdownEditor } from './components/markdown-editor/markdown-editor';
-
-export { markdownTab } from './components/tabs/markdown-tab';
-
-export { previewTab } from './components/tabs/preview-tab';
